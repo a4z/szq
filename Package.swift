cccc37ec1ff6213f91e875e8b3f2a8571db92f35
a4z <harald.achitz@gmail.com>
2025-04-06 13:46:18 +0200

    Lint also other files

--- Package.swift
+++ Package.swift
@@ -3,38 +3,37 @@
 import PackageDescription
 
 let package = Package(
-    name: "szq",
-    // other/older might be possible, but not tested
-    platforms: [
-        .macOS(.v14),
-        .iOS(.v17),
-        .watchOS(.v10),
-        .tvOS(.v16)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "szq",
-            targets: ["szq"]),
-    ],
-    targets: [
-        .binaryTarget(
-            name: "zmq",
-            url: "https://github.com/a4z/libzmq-xcf/releases/download/v4.3.5-250103_1/libzmq.xcframework.zip",
-            checksum: "34bf6c91c7151bfd9e0bea70fdea3b375246520677e0b6aa9b36184315aa0ec9"
-        ),
-        .target(
-            name: "szq",
-            dependencies: ["zmq"],
-           cxxSettings: [
-            ],
-            linkerSettings: [
-                .linkedLibrary("c++")
-            ]
-            ),
-        .testTarget(
-            name: "szqTests",
-            dependencies: ["szq"]
-        ),
-    ]
+  name: "szq",
+  // other/older might be possible, but not tested
+  platforms: [
+    .macOS(.v14),
+    .iOS(.v17),
+    .watchOS(.v10),
+    .tvOS(.v16),
+  ],
+  products: [
+    .library(
+      name: "szq",
+      targets: ["szq"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "zmq",
+      url:
+        "https://github.com/a4z/libzmq-xcf/releases/download/v4.3.5-250103_1/libzmq.xcframework.zip",
+      checksum: "34bf6c91c7151bfd9e0bea70fdea3b375246520677e0b6aa9b36184315aa0ec9"
+    ),
+    .target(
+      name: "szq",
+      dependencies: ["zmq"],
+      cxxSettings: [],
+      linkerSettings: [
+        .linkedLibrary("c++")
+      ]
+    ),
+    .testTarget(
+      name: "szqTests",
+      dependencies: ["szq"]
+    ),
+  ]
 )
--- Sources/quickcheck/main.swift
+++ Sources/quickcheck/main.swift
@@ -1,8 +1,6 @@
+// playground for quickly testing some code,
+// will be removed in the future,
+
 import szq
 
 let ctx = Context()
-
-
-
-
-
